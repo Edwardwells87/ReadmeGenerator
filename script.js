2c22f167f585149bf230bcc05084abb6d2a26e3a
Edward Wells <edward.wells87@gmail.com>
2023-04-28 22:03:53 -0500

    final adjustments after making video and actually reading the requirements

--- script.js
+++ script.js
@@ -19,27 +19,86 @@ function imagePooter(answers) {
 
 function licenseMaker(answers) {
   if (answers.License == 'GPL 3.0') {
-    return `## License
-      ![license](https://img.shields.io/badge/License-LGPL_v3-blue.svg)`;
+    return `![license](https://img.shields.io/badge/License-LGPL_v3-blue.svg)
+      
+    `;
   } else if (answers.License == "MIT") {
     console.log("License is MIT");
-    return `## License
-      ![license](https://img.shields.io/badge/License-MIT-yellow.svg)`;
+    return `![license](https://img.shields.io/badge/License-MIT-yellow.svg)
+     
+  `;
   }
   else if (answers.License == "APACHE 2.0") {
     console.log("License is APACHE 2.0");
-    return `## License
-      ![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)`;
+    return ` ![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)
+    
+    `;
   } else {
-    return ` ## License 
-      None`
+    return `
+  `
   }
 
+}
+function licenseStatements(answers){
+  if (answers.License == 'GPL 3.0') {
+    return `## License
+   - This project is licensed under the terms of the GPL 3.0 license.
+   
+   ##Contact
+     
+   - My github link is: https://github.com/${answers.contact2}
+   - Feel free to contact me at ${answers.contact} if you have more questions regarding the use of this app.`;
+  } else if (answers.License == "MIT") {
+    
+    return `## License
+   - Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE. 
+
+##Contact
+     
+    - My github link is: https://github.com/${answers.contact2}
+    - Feel free to contact me at ${answers.contact} if you have more questions regarding the use of this app.`;
+  }
+  else if (answers.License == "APACHE 2.0") {
+   
+    return `## License
+    - Apache License Version 2.0, January 2004 http://www.apache.org/licenses/
+   
+   
+    ##Contact
+     
+    - My github link is: https://github.com/${answers.contact2}
+    - Feel free to contact me at ${answers.contact} if you have more questions regarding the use of this app.`;
+  } else {
+    return ` ## License 
+     - None
+
+     ##Contact
+     
+     - My github link is: https://github.com/${answers.contact2}
+     - Feel free to contact me at ${answers.contact} if you have more questions regarding the use of this app.`
+  }
 }
 
 
 function generateReadme(answers) {
-  return `# ${answers.title1}
+  return `
+  # ${answers.title1}
 
 
   ## Table of Contents 
@@ -49,6 +108,8 @@ function generateReadme(answers) {
  - [Usage](#usage)
  - [Credits](#credits)
  - [License](#license)
+ - [Contact](#contact)
+
 
 
 ## Description 
@@ -68,7 +129,11 @@ function generateReadme(answers) {
 
 - ${answers.usageOne}
 
- `
+
+## Credits
+
+- ${answers.Credits}
+`
 }
 
 
@@ -136,16 +201,28 @@ inquirer
       name: 'License',
       message: 'Please pick one of the available license options from this list:',
       choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'None']
+    },
+
+    {
+      type: 'input',
+      message: 'Please provide an email to associate with the project for support',
+      name: 'contact',
+    },
+    {
+      type: 'input',
+      message: 'please provide a github username:',
+      name: 'contact2'
     }
 
 
   ])
   .then((answers) => {
     console.log(answers)
+    const licenseBadge = licenseMaker(answers);
     const imagePooterInner = imagePooter(answers);
     const readmecontent = generateReadme(answers);
-    const licenseMakerCont = licenseMaker(answers);
-    fs.writeFile('Readme.md', readmecontent + licenseMakerCont + imagePooterInner, (err) =>
+    const licenseMakerStuff = licenseStatements(answers);
+    fs.writeFile('Readme.md', licenseBadge + readmecontent + imagePooterInner + licenseMakerStuff, (err) =>
       err ? console.log(err) : console.log('Successfully created that readme!! ')
     );
   });
